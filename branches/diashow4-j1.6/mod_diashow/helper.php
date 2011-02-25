55a8233bc85db9fcb5a6be764d0b55d739371aab
loadbrain <loadbrain@e7b4f2b2-e666-0410-8eae-d079e3e6cd1b>
2011-02-25 12:56:15 +0000

    startet with frontend module

--- branches/diashow4-j1.6/mod_diashow/helper.php
+++ branches/diashow4-j1.6/mod_diashow/helper.php
@@ -1,35 +1,26 @@
 <?php
 /**
-* @version		$Id: helper.php 8813 2007-09-09 22:05:30Z hackwar $
-* @package		Joomla
-* @copyright	Copyright (C) 2005 - 2007 Open Source Matters. All rights reserved.
-* @license		GNU/GPL, see LICENSE.php
-* Joomla! is free software. This version may have been modified pursuant
-* to the GNU General Public License, and as distributed it includes or
-* is derivative of works licensed under the GNU General Public License or
-* other free or open source software licenses.
-* See COPYRIGHT.php for copyright notices and details.
-*/
+ * @version		$Id: helper.php 20228 2011-01-10 00:52:54Z eddieajau $
+ * @package		Joomla.Site
+ * @subpackage	mod_stats
+ * @copyright	Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
 
 // no direct access
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
 
+/**
+ * @package		Joomla.Site
+ * @subpackage	mod_stats
+ * @since		1.5
+ */
 class modDiashowHelper
 {
-	public function getDiaShowData(&$params){
-		global $mainframe;
-
-		$db	=& JFactory::getDBO();
-		$Itemid = JRequest::getVar('Itemid', 1, 'get', 'int');
-
-		$query = "SELECT * FROM #__diashow, #__diashow_visibility WHERE (#__diashow_visibility.menu_id = '" . $Itemid . "' and #__diashow.id = #__diashow_visibility.diashow_id) or (#__diashow_visibility.menu_id = '0' and #__diashow.id = #__diashow_visibility.diashow_id) and published = '1' order by ordering";
-
-		$db->setQuery($query);
-		$rows = $db->loadObjectList();
-        if ($params->get( 'optionalRandomOrder') == "R"){
-            shuffle(&$rows);
-        }
-
-		return $rows;
+	static function &getList(&$params)
+	{
+		$app	= JFactory::getApplication();
+		$db		= JFactory::getDbo();
+	
 	}
 }
