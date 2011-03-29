11579d2a759d3617cf8f38a16136ad8824d52df9
loadbrain <loadbrain@e7b4f2b2-e666-0410-8eae-d079e3e6cd1b>
2011-03-29 11:04:57 +0000

    inserted PH1-PHP Headers missing GPL License Notice

--- branches/diashow4-j1.6/com_diashow/admin/views/diashows/view.html.php
+++ branches/diashow4-j1.6/com_diashow/admin/views/diashows/view.html.php
@@ -1,6 +1,15 @@
 <?php
-// No direct access to this file
-defined('_JEXEC') or die('Restricted access');
+/*------------------------------------------------------------------------
+# com_diashow - DiaShow4
+# ------------------------------------------------------------------------
+# author Ralf Weber
+# copyright Copyright (C) 2011 http://www.weberr.de/. All Rights Reserved.
+# @license - http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+# Websites: http://www.weberr.de/
+# Technical Support: Forum - http://www.weberr.de/
+-------------------------------------------------------------------------*/
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' ); 
 
 // import Joomla view library
 jimport('joomla.application.component.view');
