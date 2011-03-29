9ad5a2125d28e6e4e53ea2a51e81140706e268ac
Ralf Weber <ralf@weberr.de>
2011-03-29 10:59:18 +0000

    inserted PH1-PHP Headers missing GPL License Notice

--- branches/diashow4-j1.6/mod_diashow/mod_diashow.php
+++ branches/diashow4-j1.6/mod_diashow/mod_diashow.php
@@ -1,17 +1,15 @@
 <?php
-/**
-* @version		4.00
-* @copyright	Copyright (C) 2008 LoadBrain <http://www.loadbrain.de/>. All rights reserved.
-* @license		GNU/GPL, see LICENSE.php
-* Joomla! is free software. This version may have been modified pursuant
-* to the GNU General Public License, and as distributed it includes or
-* is derivative of works licensed under the GNU General Public License or
-* other free or open source software licenses.
-* See COPYRIGHT.php for copyright notices and details.
-*/
-
+/*------------------------------------------------------------------------
+# mod_diashow - DiaShow4
+# ------------------------------------------------------------------------
+# author Ralf Weber
+# copyright Copyright (C) 2011 http://www.weberr.de/. All Rights Reserved.
+# @license - http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+# Websites: http://www.weberr.de/
+# Technical Support: Forum - http://www.weberr.de/
+-------------------------------------------------------------------------*/
 // no direct access
-defined('_JEXEC') or die('Restricted access');
+defined( '_JEXEC' ) or die( 'Restricted access' ); 
 
 require_once (dirname(__FILE__).DS.'helper.php');
 $diaShowData = modDiashowHelper::getDiaShowData($params);
