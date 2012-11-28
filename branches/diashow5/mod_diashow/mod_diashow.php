ed30eb5e5f6be44676a49a245939bfdaf691b22e
loadbrain <loadbrain@e7b4f2b2-e666-0410-8eae-d079e3e6cd1b>
2012-11-28 14:43:01 +0000

    Diashow5 for Joomla 3 - Initial Version

--- branches/diashow5/mod_diashow/mod_diashow.php
+++ branches/diashow5/mod_diashow/mod_diashow.php
@@ -1,6 +1,6 @@
 <?php
 /*------------------------------------------------------------------------
-# mod_diashow - DiaShow4
+# mod_diashow - DiaShow5 - Joomla 3.x
 # ------------------------------------------------------------------------
 # author Ralf Weber
 # copyright Copyright (C) 2011 http://www.weberr.de/. All Rights Reserved.
